8dbd44486acfcf2a1959cca2a08c6ac2070182ed
Klim <klim_somov@mail.ru>
2022-09-25 17:39:06 +0300

    refactoring bug with equal squares

--- src/App.vue
+++ src/App.vue
@@ -25,7 +25,6 @@
     <div v-if="outcomeSum">
       блоков для постройки перегородки потребуется: {{ outcomeSum }} шт.
     </div>
-    <div v-else-if="outcomeSum > 0">кажется ошибка в измерении проема</div>
     <div v-if="outcomeWeight">общий вес плит: {{ outcomeWeight }} кг.</div>
   </div>
 </template>
@@ -54,9 +53,12 @@ export default defineComponent({
     const outcomeSum = ref();
     const outcomeWeight = ref();
     function calculateOutcome() {
-      const result = Math.ceil(
-        (wallWidth.value * wallHeight.value) / 0.3335 - openingsSquare.value
-      );
+      const wallS = wallWidth.value * wallHeight.value;
+      const result = Math.ceil(wallS / 0.3335 - openingsSquare.value);
+      if (wallS === openingsSquare.value) {
+        return;
+      }
+
       result < 0 ? (outcomeSum.value = "") : (outcomeSum.value = result);
       outcomeWeight.value = outcomeSum.value * 36;
     }
